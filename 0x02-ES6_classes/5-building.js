17fb9f8c567aa53f6dd30461afcddafa91812089
Gideon5348 <gideonmutai096@gmail.com>
2024-05-30 14:27:56 +0000

    5. A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,21 +1,18 @@
-class Building {
+export default class Building {
   constructor(sqft) {
-    if (this.constructor === Building) {
+    if (new.target === Building) {
       throw new Error('Building is an abstract class and cannot be instantiated directly');
     }
     this._sqft = sqft;
   }
 
-  // Getter for sqft
   get sqft() {
     return this._sqft;
   }
 
-  // Abstract method to be overridden by subclasses
-  // eslint-disable-next-line class-methods-use-this
   evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (!this) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 }
-
-export default Building;
