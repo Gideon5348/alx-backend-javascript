76868e7cf57326a17cae1344d4925c1a69f648de
Gideon5348 <gideonmutai096@gmail.com>
2024-06-05 12:57:17 +0000

    0x04. Typescript

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,41 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const teacher1: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: true,
+  location: 'New York',
+  contract: true,
+};
+
+const teacher2: Teacher = {
+  firstName: 'Jane',
+  lastName: 'Smith',
+  fullTimeEmployee: false,
+  location: 'Los Angeles',
+  yearsOfExperience: 5,
+  contract: false,
+};
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(teacher1);
+console.log(teacher2);
+console.log(director1);
